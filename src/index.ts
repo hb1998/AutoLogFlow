7161bd314c3ed15dcab4e65b1c74ee4927e12e2b
hb1998 <rhabeeb1998@gmail.com>
2023-06-11 11:58:16 +0530

    chore: added empty webhook listener

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,13 @@ import { ApplicationFunctionOptions, Probot } from "probot";
 export = (app: Probot, { getRouter }: ApplicationFunctionOptions) => {
   const jiraRouter = getRouter("/jira");
 
+  jiraRouter.get("/hook", async (req, res) => {
+    res.send("successfully recieved event");
+  });
+
   app.on("pull_request", async (context) => {
     console.log(context.pullRequest())
   });
-
 
 };
 
