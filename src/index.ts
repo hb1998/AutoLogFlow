3dfcb97f2d3d2437b37495f59c27e5948272f747
hb1998 <rhabeeb1998@gmail.com>
2023-06-11 11:50:23 +0530

    chore: added api folder for vercel

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import { ApplicationFunctionOptions, Probot } from "probot";
 
 
 export = (app: Probot, { getRouter }: ApplicationFunctionOptions) => {
-  const router = getRouter("/app");
-
   const jiraRouter = getRouter("/jira");
-
 
   app.on("pull_request", async (context) => {
     console.log(context.pullRequest())
